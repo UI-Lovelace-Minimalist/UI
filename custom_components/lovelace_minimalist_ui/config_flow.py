e98822eff30684a7abc18efa85b415495e0d0ac4
Stokkie90 <rickstokking@gmail.com>
2021-12-29 18:38:23 +0000

    Fixed some minor issues

--- custom_components/lovelace_minimalist_ui/config_flow.py
+++ custom_components/lovelace_minimalist_ui/config_flow.py
@@ -1,25 +1,18 @@
 import logging
-import voluptuous as vol
 
+import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.core import callback
 from homeassistant.helpers import config_validation as cv
 
-from .const import DOMAIN, NAME
+from .const import DOMAIN
+from .const import NAME
 
 _LOGGER: logging.Logger = logging.getLogger(__package__)
 
 # Configuration:
 LANGUAGE = "language"
-LANGUAGES = [
-    "English",
-    "German",
-    "Spanish",
-    "French",
-    "Italian",
-    "Swedish",
-    "Dutch""
-]
+LANGUAGES = ["English", "German", "Spanish", "French", "Italian", "Swedish", "Dutch"]
 
 CONF_INCLUDE_OTHER_CARDS = "include_other_cards"
 DEFAULT_INCLUDE_OTHER_CARDS = False
@@ -32,8 +25,9 @@ THEME_OPTIONS = [
     "minimalist-mobile-dark",
     "minimalist-desktop-light",
     "minimalist-desktop-dark",
-    "HA selected theme"
+    "HA selected theme",
 ]
+
 
 @config_entries.HANDLERS.register(DOMAIN)
 class LovelaceMinimalistUiConfigFlow(config_entries.ConfigFlow):
@@ -47,24 +41,36 @@ class LovelaceMinimalistUiConfigFlow(config_entries.ConfigFlow):
     def async_get_options_flow(config_entry):
         return LovelaceMinimalistUiEditFlow(config_entry)
 
+
 class LovelaceMinimalistUiEditFlow(config_entries.OptionsFlow):
     def __init__(self, config_entry):
         self.config_entry = config_entry
 
+    # TODO: Needs form also on initial installation!
     async def async_step_init(self, user_input=None):
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
 
         schema = {
-            vol.Optional(LANGUAGE, default=self.config_entry.options.get("language", "English")): vol.In(LANGUAGES),
-            vol.Optional(SIDEPANEL_TITLE, default=self.config_entry.options.get("sidepanel_title", NAME)): str,
-            vol.Optional(SIDEPANEL_ICON, default=self.config_entry.options.get("sidepanel_icon", "mdi:flower")): str,
-            vol.Optional(CONF_INCLUDE_OTHER_CARDS, default=self.config_entry.options.get(CONF_INCLUDE_OTHER_CARDS, DEFAULT_INCLUDE_OTHER_CARDS)): cv.boolean,
+            vol.Optional(
+                LANGUAGE, default=self.config_entry.options.get("language", "English")
+            ): vol.In(LANGUAGES),
+            vol.Optional(
+                SIDEPANEL_TITLE,
+                default=self.config_entry.options.get("sidepanel_title", NAME),
+            ): str,
+            vol.Optional(
+                SIDEPANEL_ICON,
+                default=self.config_entry.options.get("sidepanel_icon", "mdi:flower"),
+            ): str,
+            vol.Optional(
+                CONF_INCLUDE_OTHER_CARDS,
+                default=self.config_entry.options.get(
+                    CONF_INCLUDE_OTHER_CARDS, DEFAULT_INCLUDE_OTHER_CARDS
+                ),
+            ): cv.boolean,
             # Not working yet
             # vol.Optional(THEME, default=self.config_entry.options.get("theme", "minimalist-desktop-dark")): vol.In(THEME_OPTIONS),
         }
 
-        return self.async_show_form(
-            step_id="init",
-            data_schema=vol.Schema(schema)
-        )
+        return self.async_show_form(step_id="init", data_schema=vol.Schema(schema))
--- custom_components/lovelace_minimalist_ui/process_yaml.py
+++ custom_components/lovelace_minimalist_ui/process_yaml.py
@@ -1,16 +1,17 @@
-import logging
-import yaml
-import os
-import json
 import io
-from collections import OrderedDict
+import json
+import logging
+import os
 import shutil
+from collections import OrderedDict
+
 import jinja2
-
-from homeassistant.util.yaml import Secrets, loader
+import yaml
 from homeassistant.exceptions import HomeAssistantError
+from homeassistant.util.yaml import loader
 
-from .const import DOMAIN, VERSION
+from .const import DOMAIN
+from .const import VERSION
 
 _LOGGER: logging.Logger = logging.getLogger(__package__)
 
@@ -18,24 +19,26 @@ _LOGGER: logging.Logger = logging.getLogger(__package__)
 def fromjson(value):
     return json.loads(value)
 
+
 jinja = jinja2.Environment(loader=jinja2.FileSystemLoader("/"))
 
-jinja.filters['fromjson'] = fromjson
+jinja.filters["fromjson"] = fromjson
 
 lovelace_minimalist_ui_config = {}
 lovelace_minimalist_ui_global = {}
 
 LANGUAGES = {
     "English": "EN",
-    "German":  "DE",
+    "German": "DE",
     "Spanish": "ES",
-    "French":  "FR",
+    "French": "FR",
     "Italian": "IT",
     "Swedish": "SE",
-    "Dutch":   "NL"
+    "Dutch": "NL",
 }
 
-def load_yamll(fname, secrets = None, args={}):
+
+def load_yamll(fname, secrets=None, args={}):
     try:
         process_yaml = False
         with open(fname, encoding="utf-8") as f:
@@ -43,22 +46,39 @@ def load_yamll(fname, secrets = None, args={}):
                 process_yaml = True
         if process_yaml:
             _LOGGER.warning("PARSING JINJA TEMPLATE")
-            stream = io.StringIO(jinja.get_template(fname).render({
-                **args,
-                "_lmu_config": lovelace_minimalist_ui_config,
-                "_lmu_global": lovelace_minimalist_ui_global,
-            }))
+            stream = io.StringIO(
+                jinja.get_template(fname).render(
+                    {
+                        **args,
+                        "_lmu_config": lovelace_minimalist_ui_config,
+                        "_lmu_global": lovelace_minimalist_ui_global,
+                    }
+                )
+            )
             stream.name = fname
-            return loader.yaml.load(stream, Loader=lambda _stream: loader.SafeLineLoader(_stream, secrets)) or OrderedDict()
+            return (
+                loader.yaml.load(
+                    stream,
+                    Loader=lambda _stream: loader.SafeLineLoader(_stream, secrets),
+                )
+                or OrderedDict()
+            )
         else:
             with open(fname, encoding="utf-8") as config_file:
-                return loader.yaml.load(config_file, Loader=lambda stream: loader.SafeLineLoader(stream, secrets)) or OrderedDict()
+                return (
+                    loader.yaml.load(
+                        config_file,
+                        Loader=lambda stream: loader.SafeLineLoader(stream, secrets),
+                    )
+                    or OrderedDict()
+                )
     except loader.yaml.YAMLError as exc:
         _LOGGER.error(str(exc))
         raise HomeAssistantError(exc)
     except UnicodeDecodeError as exc:
         _LOGGER.error("Unable to read file %s: %s", fname, exc)
         raise HomeAssistantError(exc)
+
 
 def _include_yaml(ldr, node):
     args = {}
@@ -68,10 +88,13 @@ def _include_yaml(ldr, node):
         fn, args, *_ = ldr.construct_sequence(node)
     fname = os.path.abspath(os.path.join(os.path.dirname(ldr.name), fn))
     try:
-        return loader._add_reference(load_yamll(fname, ldr.secrets, args=args), ldr, node)
+        return loader._add_reference(
+            load_yamll(fname, ldr.secrets, args=args), ldr, node
+        )
     except FileNotFoundError as exc:
-        _LOGGER.error("Unable to include file %s: %s", fname, exc);
+        _LOGGER.error("Unable to include file %s: %s", fname, exc)
         raise HomeAssistantError(exc)
+
 
 loader.load_yaml = load_yamll
 loader.SafeLineLoader.add_constructor("!include", _include_yaml)
@@ -82,8 +105,9 @@ def compose_node(self, parent, index):
         event = self.get_event()
         anchor = event.anchor
         if anchor not in self.anchors:
-            raise yaml.composer.ComposerError(None, None, "found undefined alias %r"
-                    % anchor, event.start_mark)
+            raise yaml.composer.ComposerError(
+                None, None, "found undefined alias %r" % anchor, event.start_mark
+            )
         return self.anchors[anchor]
     event = self.peek_event()
     anchor = event.anchor
@@ -97,11 +121,30 @@ def compose_node(self, parent, index):
     self.ascend_resolver()
     return node
 
+
 yaml.composer.Composer.compose_node = compose_node
 
-def process_yaml(hass, config_entry):
 
-    _LOGGER.warning('Start of function to process all yaml files!')
+def process_yaml(hass, config_entry):
+    _LOGGER.debug("Checking dependencies")
+    if not os.path.exists(hass.config.path("custom_components/browser_mod")):
+        _LOGGER.error('HACS Integration repo "browser mod" is not installed!')
+
+    if "include_other_cards" in config_entry.options:
+        if not config_entry.options["include_other_cards"]:
+            depenceny_resource_paths = [
+                "button-card",
+                "light-entity-card",
+                "lovelace-card-mod",
+                "mini-graph-card",
+                "mini-media-player",
+                "my-cards",
+            ]
+            for p in depenceny_resource_paths:
+                if not os.path.exists(hass.config.path(f"www/community/{p}")):
+                    _LOGGER.error(f'HACS Frontend repo "{p}" is not installed!')
+
+    _LOGGER.warning("Start of function to process all yaml files!")
 
     # Create config dir
     os.makedirs(hass.config.path(f"{DOMAIN}/configs"), exist_ok=True)
@@ -109,11 +152,15 @@ def process_yaml(hass, config_entry):
 
     if os.path.exists(hass.config.path(f"{DOMAIN}/configs")):
         # Create combined cards dir
-        combined_cards_dir = hass.config.path(f"custom_components/{DOMAIN}/__minimalist_ui__/button-cards-templates")
+        combined_cards_dir = hass.config.path(
+            f"custom_components/{DOMAIN}/__minimalist_ui__/button-cards-templates"
+        )
         os.makedirs(combined_cards_dir, exist_ok=True)
 
         # Main config
-        for fname in loader._find_files(hass.config.path(f"{DOMAIN}/configs"), "*.yaml"):
+        for fname in loader._find_files(
+            hass.config.path(f"{DOMAIN}/configs"), "*.yaml"
+        ):
             loaded_yaml = load_yamll(fname)
             if isinstance(loaded_yaml, dict):
                 lovelace_minimalist_ui_config.update(loaded_yaml)
@@ -129,24 +176,31 @@ def process_yaml(hass, config_entry):
 
         # Copy chosen language file over to config dir
         shutil.copy2(
-            hass.config.path(f"custom_components/{DOMAIN}/lovelace/translations/{language}.yaml"),
-            hass.config.path(f"{combined_cards_dir}/{language}.yaml")
+            hass.config.path(
+                f"custom_components/{DOMAIN}/lovelace/translations/{language}.yaml"
+            ),
+            hass.config.path(f"{combined_cards_dir}/{language}.yaml"),
         )
         # Copy over cards from integration
         shutil.copytree(
-            hass.config.path(f"custom_components/{DOMAIN}/lovelace/button-cards-templates"),
+            hass.config.path(
+                f"custom_components/{DOMAIN}/lovelace/button-cards-templates"
+            ),
             hass.config.path(f"{combined_cards_dir}"),
-            dirs_exist_ok=True
+            dirs_exist_ok=True,
         )
         shutil.copytree(
             hass.config.path(f"{DOMAIN}/cards"),
             hass.config.path(f"{combined_cards_dir}/custom_cards"),
-            dirs_exist_ok=True
+            dirs_exist_ok=True,
         )
 
         # Load Themes
         themes = OrderedDict()
-        for fname in loader._find_files(hass.config.path(f"custom_components/{DOMAIN}/lovelace/themefiles"), "*.yaml"):
+        for fname in loader._find_files(
+            hass.config.path(f"custom_components/{DOMAIN}/lovelace/themefiles"),
+            "*.yaml",
+        ):
             loaded_yaml = load_yamll(fname)
             if isinstance(loaded_yaml, dict):
                 themes.update(loaded_yaml)
@@ -154,8 +208,7 @@ def process_yaml(hass, config_entry):
 
                 os.makedirs(hass.config.path(f"themes/{theme_name}"), exist_ok=True)
                 shutil.copy2(
-                    fname,
-                    hass.config.path(f"themes/{theme_name}/{theme_name}.yaml")
+                    fname, hass.config.path(f"themes/{theme_name}/{theme_name}.yaml")
                 )
 
         if "theme" in config_entry.options:
@@ -194,25 +247,27 @@ def process_yaml(hass, config_entry):
 
         if not os.path.exists(path):
             _LOGGER.debug("Create .installed file")
-            open(path, 'w').close()
+            open(path, "w").close()
 
         reload_configuration(hass)
 
     hass.services.async_register(DOMAIN, "installed", handle_installed)
+
 
 def reload_configuration(hass):
     if os.path.exists(hass.config.path(f"{DOMAIN}/configs")):
         # Main config
         # No config generated yet at the start of process_yaml()
         config_new = OrderedDict()
-        for fname in loader._find_files(hass.config.path(f"{DOMAIN}/configs/"), "*.yaml"):
+        for fname in loader._find_files(
+            hass.config.path(f"{DOMAIN}/configs/"), "*.yaml"
+        ):
             _LOGGER.warning(f"Loading file: {fname}")
             loaded_yaml = load_yamll(fname)
             if isinstance(loaded_yaml, dict):
                 config_new.update(loaded_yaml)
 
         lovelace_minimalist_ui_config.update(config_new)
-
 
         if os.path.exists(hass.config.path(f"custom_components/{DOMAIN}/.installed")):
             installed = "true"
