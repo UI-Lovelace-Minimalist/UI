5791794a03e1aed08e0951dfdf61299afd4d3d23
Rick Stokkingreef <rickstokking@gmail.com>
2021-12-30 20:43:00 +0100

    Update config_flow.py

--- custom_components/lovelace_minimalist_ui/config_flow.py
+++ custom_components/lovelace_minimalist_ui/config_flow.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import logging
 from homeassistant.components.frontend import DEFAULT_THEME_COLOR
 
@@ -132,52 +134,3 @@ class LmuOptionFlowHandler(config_entries.OptionsFlow):
 
         return self.async_show_form(step_id="user", data_schema=vol.Schema(schema))
 
-
-
-
-
-# @config_entries.HANDLERS.register(DOMAIN)
-# class LovelaceMinimalistUiConfigFlow(config_entries.ConfigFlow):
-#     async def async_step_user(self, user_input=None):
-#         if self._async_current_entries():
-#             return self.async_abort(reason="single_instance_allowed")
-#         return self.async_create_entry(title=NAME, data={})
-
-#     @staticmethod
-#     @callback
-#     def async_get_options_flow(config_entry):
-#         return LovelaceMinimalistUiEditFlow(config_entry)
-
-
-# class LovelaceMinimalistUiEditFlow(config_entries.OptionsFlow):
-#     def __init__(self, config_entry):
-#         self.config_entry = config_entry
-
-#     # TODO: Needs form also on initial installation!
-#     async def async_step_init(self, user_input=None):
-#         if user_input is not None:
-#             return self.async_create_entry(title="", data=user_input)
-
-#         schema = {
-#             vol.Optional(
-#                 LANGUAGE, default=self.config_entry.options.get("language", "English")
-#             ): vol.In(LANGUAGES),
-#             vol.Optional(
-#                 SIDEPANEL_TITLE,
-#                 default=self.config_entry.options.get("sidepanel_title", NAME),
-#             ): str,
-#             vol.Optional(
-#                 SIDEPANEL_ICON,
-#                 default=self.config_entry.options.get("sidepanel_icon", "mdi:flower"),
-#             ): str,
-#             vol.Optional(
-#                 CONF_INCLUDE_OTHER_CARDS,
-#                 default=self.config_entry.options.get(
-#                     CONF_INCLUDE_OTHER_CARDS, DEFAULT_INCLUDE_OTHER_CARDS
-#                 ),
-#             ): cv.boolean,
-#             # Not working yet
-#             # vol.Optional(THEME, default=self.config_entry.options.get("theme", "minimalist-desktop-dark")): vol.In(THEME_OPTIONS),
-#         }
-
-#         return self.async_show_form(step_id="init", data_schema=vol.Schema(schema))
