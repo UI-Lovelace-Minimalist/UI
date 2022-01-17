bab838abc3265add9f7713f5384766cbb5dcf4e1
rick.stokkingreef <rick.stokkingreef@takeaway.com>
2022-01-17 15:47:47 +0000

    Updated Devcontainer & fixed reload of custom_cards

--- custom_components/ui_lovelace_minimalist/process_yaml.py
+++ custom_components/ui_lovelace_minimalist/process_yaml.py
@@ -261,6 +261,9 @@ def process_yaml(hass: HomeAssistant, ulm: UlmBase):
 
 def reload_configuration(hass):
     """Reload Configuration."""
+    combined_cards_dir = hass.config.path(
+        f"custom_components/{DOMAIN}/__ui_minimalist__/ulm_templates"
+    )
     if os.path.exists(hass.config.path(f"{DOMAIN}/configs")):
         # Main config
         # No config generated yet at the start of process_yaml()
@@ -284,6 +287,14 @@ def reload_configuration(hass):
             [
                 ("installed", installed),
             ]
+        )
+
+    if os.path.exists(hass.config.path(f"{DOMAIN}/custom_cards")):
+        # Copy over manually installed custom_cards from user
+        shutil.copytree(
+            hass.config.path(f"{DOMAIN}/custom_cards"),
+            hass.config.path(f"{combined_cards_dir}/custom_cards"),
+            dirs_exist_ok=True,
         )
 
     hass.bus.async_fire("ui_lovelace_minimalist_reload")
