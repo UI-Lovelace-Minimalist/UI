b2ad718095c16441d630e08c96aab141cf8775af
SNoof85 <snoof85@gmail.com>
2022-04-01 12:17:00 +0200

    Bump card mod
    Disable april fool 🍡

--- scripts/update-cards.py
+++ scripts/update-cards.py
@@ -7,7 +7,7 @@ cards = (
     ('light-entity-card', 'https://github.com/ljmerza/light-entity-card/releases/download/4.5.0/light-entity-card.js'),
     ('lovelace-auto-entities', 'https://raw.githubusercontent.com/thomasloven/lovelace-auto-entities/1.11.0/auto-entities.js'),
     # card-mod: https://github.com/thomasloven/lovelace-card-mod/tree/3.1.2#performance-improvements
-    ('lovelace-card-mod', 'https://raw.githubusercontent.com/thomasloven/lovelace-card-mod/3.1.2/card-mod.js'),
+    ('lovelace-card-mod', 'https://raw.githubusercontent.com/thomasloven/lovelace-card-mod/3.1.4/card-mod.js'),
     ('mini-graph-card', 'https://github.com/kalkih/mini-graph-card/releases/download/v0.11.0-dev.5/mini-graph-card-bundle.js'),
     ('mini-media-player', 'https://github.com/kalkih/mini-media-player/releases/download/v1.16.2/mini-media-player-bundle.js'),
     ('my-cards', 'https://raw.githubusercontent.com/AnthonMS/my-cards/main/dist/my-cards.js'),
