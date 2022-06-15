02d7e46b99b2ab3fe71f1a949085d4dbad8bbbb4
schumijo <j.joussot@free.fr>
2022-06-15 12:07:28 +0200

    Fix folder

--- custom_components/ui_lovelace_minimalist/configure.py
+++ custom_components/ui_lovelace_minimalist/configure.py
@@ -69,7 +69,7 @@ def configure_cards(hass: HomeAssistant, ulm: UlmBase):
                 hass.config.path(
                     f"custom_components/{DOMAIN}/lovelace/custom_actions.yaml"
                 ),
-                hass.config.path(f"{DOMAIN}/custom_actions.yaml"),
+                hass.config.path(f"{DOMAIN}/config/custom_actions.yaml"),
             )
         # Copy chosen language file over to config dir
         shutil.copy2(
