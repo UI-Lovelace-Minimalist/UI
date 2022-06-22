411d5bb82a0ce93cc91097d242c990b62211648c
schumijo <j.joussot@free.fr>
2022-06-22 11:17:19 +0200

    Fix code quality

--- custom_components/ui_lovelace_minimalist/configure.py
+++ custom_components/ui_lovelace_minimalist/configure.py
@@ -86,8 +86,8 @@ def configure_cards(hass: HomeAssistant, ulm: UlmBase):
                 )
         # Copy example custom actions file over to user config dir if not exists
         if not os.path.exists(
-                hass.config.path(f"{DOMAIN}/custom_actions/custom_actions.yaml")
-            ):
+            hass.config.path(f"{DOMAIN}/custom_actions/custom_actions.yaml")
+        ):
             shutil.copy2(
                 hass.config.path(
                     f"custom_components/{DOMAIN}/lovelace/custom_actions.yaml"
